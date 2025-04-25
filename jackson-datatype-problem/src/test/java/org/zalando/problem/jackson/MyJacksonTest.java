d2f154b55175a0d07a8cd437c15374b0068b520b
Jenna Hartman <jenn01.619208@gmail.com>
2025-04-24 20:07:34 -0400

    committing jackson test 1

--- jackson-datatype-problem/src/test/java/org/zalando/problem/jackson/MyJacksonTest.java
+++ jackson-datatype-problem/src/test/java/org/zalando/problem/jackson/MyJacksonTest.java
@@ -1,2 +1,26 @@
+package org.zalando.problem.jackson;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
+import org.zalando.problem.StatusType;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+class MyJacksonTest {
+    @Test
+    void shouldSerializeProblemWithModule() throws Exception {
+        ObjectMapper mapper = new ObjectMapper().registerModule(new ProblemModule());
+        Problem problem = Problem.valueOf(Status.NOT_FOUND);
+        String json = mapper.writeValueAsString(problem);
+        assertThat(json).contains("\"status\":404");
+    }
+
+
+}
 
